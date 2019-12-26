4d9676438ab5f364570164cff9c17ffd853f6e6d
artur <arthurjhonys@gmail.com>
2019-12-26 08:42:48 -0400

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,70 @@
+let calculaHoras = () => {
+	
+	//let parm = document.getElementById('j').value
+
+	//console.log(`parm ${parm}`)
+
+	//let jornada = parm === '' ? 525 : parm.newRecord()
+
+	//console.log(`Jornada: ${jornada}`)
+
+	// Pega os valores dos inputs
+	let jornada = document.getElementById('j').value.newRecord()
+	let entrada1 = document.getElementById('e1').value.newRecord()
+	let saida1 = document.getElementById('s1').value.newRecord()
+	let entrada2 = document.getElementById('e2').value.newRecord()
+	
+	console.log(`jornada ${jornada}`)
+	// Define o parâmetro de Jornada de Trabalho
+	let parametroJornadaTrabalho = jornada === null ? recordFromTotalMinutes(525) : jornada
+	console.log(`parametro Jornada Trabalho: ${parametroJornadaTrabalho}`)
+	//let parametroJornadaTrabalho = jornada
+	/* Está havendo um problema na conversão da Jornada, quando o campo jornada vai vazio ele retorna um objeto causando um erro no calculo
+	dos dados */
+	console.log(`parametroJornadaTrabalho.Horas: ${parametroJornadaTrabalho.h}`)
+	console.log(`parametroJornadaTrabalho.Minutos: ${parametroJornadaTrabalho.m}`)
+	console.log(`parametroJornadaTrabalho.total: ${parametroJornadaTrabalho.total}`)
+	// Fórmula de cálculo de hora de saída
+	let minutosTrabalhado = saida1.total - entrada1.total
+	let minutosATrabalhar = parametroJornadaTrabalho.total - minutosTrabalhado
+	let totalCalculo = entrada2.total + minutosATrabalhar
+	console.log(`Minutos a trabalhar: ${minutosATrabalhar}`)
+	
+	let saida2 = recordFromTotalMinutes(totalCalculo)
+	
+	// Atualiza campo com o horário de saída
+	document.getElementById("s2").value = saida2.desc
+		
+}
+
+// Evento onClick do botão Calcular
+document.getElementById('btnCalcular').addEventListener('click',calculaHoras)
+
+// Estrutura do registro de ponto 
+var record = function (h, m, t, d){
+	this.hours = h
+	this.minutes = m
+	this.total = t
+	this.desc = d
+}
+
+// External Method para criação de um novo Registro de ponto
+String.prototype.newRecord = function(){
+	
+	if (this === "")
+		return null
+	var h = parseInt(this.split(":")[0]) 
+	var m = parseInt(this.split(":")[1])
+	var t = h * 60 + m
+	return new record(h, m, t, this)
+}
+
+// Metodo para criar um registro de ponto a partir de total de minutos
+const recordFromTotalMinutes = (total) => {
+
+	var h = Math.floor(total/60)
+	var m = total - h * 60
+	var desc = String(h).concat(":").concat(("00"+String(m)).slice(-2))
+
+	return new record(h, m, total, desc)
+}
