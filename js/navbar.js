b035acf15f18ed294012c056baa67fcbb6cce132
Marcos Oliveira <ziguifrido@users.noreply.github.com>
2023-11-23 16:39:06 -0400

    feat: Dark Mode

---
+++ js/navbar.js
@@ -0,0 +1,38 @@
+const getCookie = (name) => {
+    let key = name + "="
+    let value = ''
+    decodeURIComponent(document.cookie)
+    .split(';')
+    .forEach(c => {
+      while (c.charAt(0) == ' ')
+        c = c.substring(1)
+  
+      if (c.indexOf(key) == 0)
+        value = c.substring(key.length, c.length)
+    })
+    return value
+  }
+  
+  const cookieTheme = getCookie("theme")
+  const theme = cookieTheme !== '' ? cookieTheme : 'light'
+  
+  document.addEventListener("DOMContentLoaded", () => { 
+    ToggleDarkMode(theme)
+  })
+  
+  const ToggleMenu = () => {
+    document.getElementsByClassName("navbar-list")[0].classList.toggle('show')
+  }
+  
+  const ToggleDarkMode = (theme) => {
+    document.body.className = ''
+    document.body.classList.add(theme)
+    setCookie("theme", theme, 365)
+  }
+  
+  const setCookie = (name, value, expiration) => {
+    const d = new Date()
+    d.setTime(d.getTime() + (expiration * 24 * 60 * 60 * 1000))
+    let expires = "expires=" + d.toUTCString()
+    document.cookie = name + "=" + value + ";" + expires + ";path=/"
+  }
